18efb3e727b9198732095032f8a54f5361965483
Adam Turner <hello@adamrichardturner.dev>
2021-07-27 14:54:56 +0100

    First upload of NewsAPI app, AppBar implemented with MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import '@fontsource/roboto';
 import './App.css';
+import Layout from './components/Layout'
+import NewsCard from './components/NewsCard'
 
 function App() {
   return (
     <div className="App">
+      <Layout />
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
