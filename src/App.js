24b7505f5517f6e4dbdad713dedd2fa3f8c53f87
Adam Turner <hello@adamrichardturner.dev>
2021-08-07 22:38:34 +0100

    Added functions to consume NewsAPI

--- src/App.js
+++ src/App.js
@@ -3,23 +3,42 @@ import './App.css';
 import Container from '@material-ui/core/Container';
 import Layout from './components/Layout'
 import NewsCard from './components/NewsCard'
-
-function getNews() {
-  const key = process.env.REACT_APP_NEWSAPI_KEY
-  const url = `https://newsapi.org/v2/top-headlines?country=gb&apiKey=${key}`;
-  const req = new Request(url);
-  fetch(req)
-    .then(function(response) {
-        return response.json()
-    })
-}
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  getNews()
+
+  const [ appState, setAppState ] = useState({
+    loading: false,
+    articles: []
+  })
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const key = process.env.REACT_APP_NEWSAPI_KEY
+    const url = `https://newsapi.org/v2/top-headlines?country=gb&apiKey=${key}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((articles) => {
+        setAppState({ loading: false, articles: articles.articles });
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        setAppState({ loading: true });
+      });
+  }, [setAppState]);
+
+  const displayNews = () => {
+    const articles = appState.articles
+    console.log(typeof articles)
+  }
+
   return (
     <div className="App">
       <Container maxWidth="lg" className="Container">
         <Layout />
+        {displayNews()}
       </Container>
     </div>
   );
