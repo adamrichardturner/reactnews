520e76d0a3e20a0f9e7ca53185aa4ebe649e8e77
Adam Turner <hello@adamrichardturner.dev>
2021-08-12 12:50:36 +0100

    Added comments to App

--- src/App.js
+++ src/App.js
@@ -9,49 +9,59 @@ import placeholder from './reactnews-placeholder.jpg'
 import loading from './Rocket.gif'
 
 function App() {
-
+  // Initialise state and state setter for articles
   const [ state, setState ] = useState({
     loading: true,
     articles: {}
   })
 
+  // Initialise currentPage for changing page with AppDrawer
+
   const [ currentPage, setCurrentPage ] = useState("breaking-news")
-  
+
+  // API key for GNews API & initial endpoint
+
   const key = process.env.REACT_APP_NEWSAPI_KEY
 
   const endpoint = `top-headlines`
 
   const [ url, setUrl ] = useState(`https://gnews.io/api/v4/${endpoint}?token=${key}&country=gb`)
 
+  // Fetch API and watch URL for change dependant on clicks on the AppDrawer anchors
+
   useEffect(() => {
     fetchNews()
   }, [url] )
 
+  // Async fetch call to GNews API
+
   const fetchNews = async () => {
-    console.log(url)
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data)
     setState({
       articles: data.articles
     })
   }
+
+  // Update the page topic based on the anchor, this function is passed as props 
+  // to child Layout
 
   const updatePage = title => {
     setCurrentPage(title)
     setUrl(`https://gnews.io/api/v4/${endpoint}?token=${key}&country=gb&topic=${title}`)
   }
 
-  const searchPage = term => {
-    setUrl(`https://gnews.io/api/v4/search?q=${term}&token=${key}`)
-  }
+  // Initialise state & stateSetters for modalOpen status, article title, content, image
+  // and sourceURL
 
   const [ modalOpen, setModalOpen ] = useState(false)
-
   const [ title, setTitle ] = useState("")
   const [ content, setContent ] = useState("")
   const [ image, setImage ] = useState("")
   const [ sourceURL, setSourceURL ] = useState("")
+
+  // On opening the modal set the title, content, imageg, sourceURL and
+  // status of modal.
 
   const openModal = (title, content, image, sourceURL) => {
     setTitle(title)
@@ -59,8 +69,9 @@ function App() {
     setImage(image)
     setSourceURL(sourceURL)
     setModalOpen(true)
-    console.log(`Source URL IS!!! ${sourceURL}`)
   }
+
+  // Handle closing the modal
 
   const closeModal = () => {
     setModalOpen(false)
@@ -70,7 +81,6 @@ function App() {
     <div className="App">
       <Container maxWidth="lg" className="Container">
         <Layout updatePage={updatePage} 
-                searchPage={searchPage}
                 openModal={openModal}
         />
         <SimpleModal display={modalOpen} 
@@ -81,12 +91,15 @@ function App() {
                      source={sourceURL}
                      />
         {
+          // Conditional renedering of loading gif
           state.loading === true ? <div className="ParentLoader">
             <img className="Loader" src={loading} alt="Loading" />
           </div>
           
           :
-
+          // Iterate over the returned object from the fetch API call
+          // and returns a NewsCard for each article, dependant on the
+          // arguments given by user clicking on the AppDrawer anchors
           Object.keys(state.articles).map(function(article) {
             return <div className="newsCard">
                     <div className="card">
