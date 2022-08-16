2c5b756ae53f529bc609ce3076167cd8beddcbb8
Adam Turner <hello@adamrichardturner.dev>
2022-08-16 19:48:28 +0100

    Completed basic newscard

--- components/NewsCard/NewsCard.tsx
+++ components/NewsCard/NewsCard.tsx
@@ -1,14 +1,28 @@
 import { INewsCard } from '../../types/INewsCard';
+import styles from '../../styles/NewsCard.module.scss';
+import Image from 'next/image';
+import testImage from './test.jpeg';
 
 type Props = {
+    article: INewsCard;
+};
 
-}
-
-
-const NewsCard: React.FC<Props> = ({title, description, content, image, source}) => {
+const NewsCard: React.FC<Props> = ({article}) => {
     return(
         <>
-
+          <article className={styles.newsCard}>
+              <div className={styles.newsCard__imageContainer}>
+                  <Image 
+                    src={testImage}
+                    alt="Title for Article"
+                  />
+              </div>
+              <div className={styles.newsCard__contentContainer}>
+                  <h4>Article Title</h4>
+                  <p>Truncated content...</p>
+                  <p><span>Source:</span> NewsSource</p>
+              </div>
+          </article>
         </>
     )
 }
